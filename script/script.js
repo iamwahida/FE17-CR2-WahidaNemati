6e262c28b22ee26a65c0e20896d892e60e41310c
iamwahida <nemati.w@yahoo.com>
2022-10-01 16:03:58 +0200

    changes

--- script/script.js
+++ script/script.js
@@ -3,16 +3,16 @@
 // console.log(test);
 
 let tasks = JSON.parse(tasksToDo);
-// console.table(tasks);
+console.table(tasks);
 
-//print every to do task
+//print every toDo task
 for (let toDo of tasks) {
     document.getElementById("merlin").innerHTML +=  
         `<div class = "card border border-dark pb-3">
         <i class="fa fa-bookmark" id ="mark"></i>
         <p id ="blueTaskbutton">Task</p>
         <img src="images/${toDo.image}" width="100%" height = "50%">
-        <br>
+        <br><br>
             <div class="text-center">
                 <h6>${toDo.taskName}</h6>
                 <p>${toDo.description}</p>
@@ -25,47 +25,54 @@ for (let toDo of tasks) {
         </div>
         `
     };
-    let increase_amount = document.getElementsByClassName("amount");
 
-    //for no-repeat for every index
+let increase_amount = document.getElementsByClassName("amount");
+// var element = document.getElementsByClassName("importance");
+console.log(increase_amount.length);
+
+//for no-repeat for every index
     function increase_priority (index) {
-            // change_color(index);
-            if (tasks[index].priority != 5) {  
+        if (tasks[index].priority != 5) {
             tasks[index].priority++;
-            change_color(index);
+            // change_color(index);
             document.getElementsByClassName("importance")[index].innerHTML = tasks[index].priority;
-            }
-    };
-  //create function for the bg colors with if statements
+        }
+};   
 
-            function change_color(x)  {
-            if (tasks[x].priority == 0) {
-                document.getElementById("prio").style.backgroundColor = "green";
-            }else if (tasks[x].priority == 1) {
-                document.getElementById("prio").style.backgroundColor = "green";
-            }  
-            else if (tasks[x].priority == 2) {
-                document.getElementById("prio").style.backgroundColor = "yellow";
-            } 
-            else if (tasks[x].priority == 3) {
-                document.getElementById("prio").style.backgroundColor = "yellow";
-            } 
-            else if (tasks[x].priority > 3 ) {
-                document.getElementById("prio").style.backgroundColor = "red";
-            } 
-            else {
-                document.getElementById("prio").style.backgroundColor = "red";
-            }
-    };
+ //create function for the bg colors with if statements
+    function change_color(index) {
+        if (tasks[index].priority == 0) {
+            document.getElementsByClassName("importance")[index].style.backgroundColor = "green";
+        }else if (tasks[index].priority == 1) {
+            document.getElementsByClassName("importance")[index].style.backgroundColor = "green";
+        }  
+        else if (tasks[index].priority == 2) {
+            document.getElementsByClassName("importance")[index].style.backgroundColor = "yellow";
+        } 
+        else if (tasks[index].priority == 3) {
+            document.getElementsByClassName("importance")[index].style.backgroundColor = "yellow";
+        } 
+        else if (tasks[index].priority > 3 ) {
+            document.getElementsByClassName("importance")[index].style.backgroundColor = "red";
+        } 
+        else {
+            document.getElementsByClassName("importance")[index].style.backgroundColor = "red";
+        }
+        document.getElementsByClassName("importance")[index].innerHTML = tasks[index].priority;
+    };  
 
+    //for each card
     for (let i = 0; i < increase_amount.length; i++) {
         increase_amount[i].addEventListener("click", function(){
-            increase_priority(i);
+            increase_priority(i); 
+            change_color(i);
         })
     };
 
+ document.getElementById("click").onclick = sortByValue;
+    //sort
     function sortByValue() {
-        tasks.sort(((a, b) => a.priority - b.priority))
+        tasks.sort(((a, b) => b.priority - a.priority))
         document.getElementById("merlin").innerHTML ="";
         
         for (let toDo of tasks) {
@@ -87,11 +94,18 @@ for (let toDo of tasks) {
                 </div>
                 `
             };
+            
+            let newamount = document.getElementsByClassName("amount");
+            for (let i = 0; i < newamount.length; i++) {
+                newamount[i].addEventListener("click", function(){
+                    increase_priority(i); 
+                    change_color(i);
+                })
+            };
 
     };
 
-    document.getElementById("id").onclick = sortByValue;
-
+   
 
 
 
